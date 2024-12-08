9442c1dd30654dd24e3ed00c6825ff2855edbdea
Ahmed Raza Khan <ahmedrazakhank112@gmail.com>
2024-12-08 20:49:43 +0500

    done

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Login />} />
         <Route path="/otp-verfication" element={<OtpVerification />} />
       </Routes>
     </Router>
